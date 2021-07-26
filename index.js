0b4456ef66cf169f09768d2ee1bdfc62084b5dbf
Yucky <registyucky@gmail.com>
2021-07-26 23:24:03 +0900

    get input value

--- index.js
+++ index.js
@@ -5,4 +5,10 @@
 // save todo
 // erase todo
 // end todo
-console.log("hello");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(input.value);
+});
