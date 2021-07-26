9b26edf085c23005c38d5ca1bd6fd30a6e6414c6
Yucky <registyucky@gmail.com>
2021-07-27 00:09:55 +0900

    delete todo

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ function add(todo) {
     const li = document.createElement("li");
     li.innerText = todo_text;
     li.classList.add("list-group-item");
+    li.addEventListener("contextmenu", function (event) {
+      event.preventDefault();
+      li.remove();
+      saveData();
+    });
+
     ul.appendChild(li);
     input.value = "";
     saveData();
