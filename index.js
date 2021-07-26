9ed4008a35cb75b5eb1864739b9d3ccad2032519
Yucky <registyucky@gmail.com>
2021-07-26 23:35:50 +0900

    show function

--- index.js
+++ index.js
@@ -7,8 +7,21 @@
 // end todo
 const form = document.getElementById("form");
 const input = document.getElementById("input");
+const ul = document.getElementById("ul");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log(input.value);
+  add();
 });
+
+function add() {
+  let todo_text = input.value;
+  if (todo_text.length > 0) {
+    const li = document.createElement("li");
+    li.innerText = todo_text;
+    li.classList.add("list-group-item");
+    ul.appendChild(li);
+    input.value = "";
+  }
+}
